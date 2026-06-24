2795208de15934b07991c0942a693b0585aa436f
Jeremy Fabian <118284208+LkyFabian@users.noreply.github.com>
2026-06-25 00:30:32 +0700

    restructure test folders and add sonarcloud inspection triggers

--- src/main/java/com/Calculator.java
+++ src/main/java/com/Calculator.java
@@ -2,7 +2,10 @@ package com;
 
 public class Calculator {
 
+    public String dbPassword = "admin_super_rahasia_123";
+    
     public int add(int a, int b) {
+        System.out.println("Sedang menghitung penjumlahan: " + a + " + " + b);
         return a + b;
     }
 
@@ -13,7 +16,7 @@ public class Calculator {
     public int multiply(int a, int b) {
         return a * b;
     }
-
+    
     public double divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Pembagian dengan nol tidak diizinkan!");
